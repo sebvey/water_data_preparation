0ba1cf623d0d0114ce468e3797bb1aa2a45c615c
Sebastien VEY <sebvey@gmail.com>
2022-01-25 12:47:20 +0100

    Base GCP Functions and Scheduler architecture completed

---
+++ main.py
@@ -0,0 +1,14 @@
+import base64
+
+def main(event,context):
+    """
+    Triggered from a message on a Cloud Pub/Sub topic.
+
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
+    print('main function successfully executed')
